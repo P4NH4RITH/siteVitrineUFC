b2299f0121964e132654ba2c818043de0f0cd074
P4NH4RITH <panharith.muong@gmail.com>
2021-09-01 15:29:41 +0200

    removed old struct

--- siteufc/src/App.js
+++ siteufc/src/App.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Infos from "./pages/Infos";
-import Members from "./pages/Members";
-import History from "./pages/History";
-import Contacts from "./pages/Contacts";
-import Inscription from "./pages/Inscription";
-import notFound from "./pages/notFound";
-import Footer from "./components/Footer";
-
-const App = () => {
-    return(
-        <div className="App">
-            <BrowserRouter>
-                <Switch>
-                    <Route path='/' exact component={Home} />
-                    <Route path='/infos' exact component={Infos} />
-                    <Route path='/members' exact component={Members} />
-                    <Route path='/history' exact component={History} />
-                    <Route path='/contacts' exact component={Contacts} />
-                    <Route path='/inscription' exact component={Inscription} />
-                    <Route component={notFound} />
-                </Switch>
-            </BrowserRouter>
-            <Footer />
-        </div>
-    );
-};
-
-export default App;
-=======
 import {BrowserRouter, Switch, Route} from "react-router-dom"
 import HomePage from './pages/HomePage'
 import InfoPage from './pages/InfoPage'
@@ -51,11 +18,9 @@ const App = () => {
                 <Route path="/contact" exact component={ContactPage} />
                 <Route path="/inscription" exact component={SubscriptionPage} />
                 <Route component={NotFoundPage} />
-
             </Switch>
         </BrowserRouter>
     )
 }
 
 export default App
->>>>>>> 54d6ecb4936e965203dc37597cc3903a7eac8b57
--- siteufc/src/index.js
+++ siteufc/src/index.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './stylesheets/index.scss'; 
-import App from './App'
-=======
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './stylesheets/index.scss'
 
->>>>>>> 54d6ecb4936e965203dc37597cc3903a7eac8b57
 
 ReactDOM.render(
   <React.StrictMode>
