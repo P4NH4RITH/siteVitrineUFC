509a6c3061afcc228c498cb863adbe4945dbea29
Antoine Menskoï <menskoi.antoine@outlook.com>
2021-09-09 09:53:50 +0200

    fixed burger menu, issue was in app.js

--- siteufc/src/App.js
+++ siteufc/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useRef } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import InfoPage from "./pages/InfoPage";
@@ -6,9 +7,27 @@ import StoryPage from "./pages/StoryPage";
 import ContactPage from "./pages/ContactPage";
 import SubscriptionPage from "./pages/SubscriptionPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import { useOnClickOutside } from './hooks';
+import { Burger, Menu } from './components';
+import FocusLock from 'react-focus-lock';
 
-const App = () => {
+function App() {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  const menuId = "main-menu";
+
+  useOnClickOutside(node, () => setOpen(false));
+
+
   return (
+    <>
+    <div ref={node}>
+          <FocusLock disabled={!open}>
+            <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+            <Menu open={open} setOpen={setOpen} id={menuId} />
+          </FocusLock>
+    </div>
+
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={HomePage} />
@@ -20,7 +39,9 @@ const App = () => {
         <Route component={NotFoundPage} />
       </Switch>
     </BrowserRouter>
+    </>
   );
-};
+
+}
 
 export default App;
